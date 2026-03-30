40c17b8354245bf74da911c29534390ed8e296d0
Sanjay707077 <tsanjay7070@gmail.com>
2026-03-30 15:42:42 +0530

    [Sanjay707077] Added [UC3]

--- app/src/TrainApp.java
+++ app/src/TrainApp.java
@@ -6,27 +6,18 @@ public class TrainApp {
 
         System.out.println("=== Train Consist Management App ===");
 
-        // Step 1: Initialize ArrayList
-        ArrayList<String> bogies = new ArrayList<>();
+        // Step 1: Create HashSet
+        HashSet<String> bogieIDs = new HashSet<>();
 
-        // Step 2: Add Passenger Bogies
-        bogies.add("Sleeper");
-        bogies.add("AC Chair");
-        bogies.add("First Class");
+        // Step 2: Add Bogie IDs (with duplicates)
+        bogieIDs.add("BG101");
+        bogieIDs.add("BG102");
+        bogieIDs.add("BG103");
+        bogieIDs.add("BG101"); // duplicate
+        bogieIDs.add("BG102"); // duplicate
 
-        // Step 3: Display Bogies
-        System.out.println("\nAfter Adding Bogies:");
-        System.out.println(bogies);
-
-        // Step 4: Remove a Bogie
-        bogies.remove("AC Chair");
-
-        // Step 5: Check existence
-        boolean exists = bogies.contains("Sleeper");
-        System.out.println("\nDoes Sleeper exist? " + exists);
-
-        // Step 6: Final List
-        System.out.println("\nFinal Bogie List:");
-        System.out.println(bogies);
+        // Step 3: Display Unique IDs
+        System.out.println("\nUnique Bogie IDs:");
+        System.out.println(bogieIDs);
     }
 }
